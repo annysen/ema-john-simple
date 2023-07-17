905f99e47c23f481f9be8ebb522f9d46ec4974cb
annysen <annysen95@gmail.com>
2023-07-18 02:18:00 +0600

    added cart as stiky, and calculation all

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,39 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import "./Shop.css"
-import Product from '../Product/Product';
+import React, { useEffect, useState } from "react";
+import "./Shop.css";
+import Product from "../Product/Product";
+import Cart from "../Cart/Cart";
 const Shop = () => {
-    const [products, setProducts] = useState([])
-    
-    const [cart, setCart] = useState([])
+  const [products, setProducts] = useState([]);
 
-// load data fropm fetch 
-    useEffect(() => {
-        fetch('products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data))
-    },[])
+  const [cart, setCart] = useState([]);
 
+  // load data fropm fetch
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
-    // add to cart event handler 
-    const addToCart = (product) => {
-        const newCart = [...cart, product];
-        setCart(newCart)
-    }
-    return (
-        <div className='shop-Container'>
-            <div className='product-container'>
-          {
-            products.map(product => <Product
-            key={product.id} product={product} addToCart={addToCart}
-            ></Product>)
-          }
-            </div>
-            <div className='cart-container'>
-                <h4>Order Summary</h4>
-                <p>Total Order {cart.length}</p>
-            </div>
-        </div>
-    );
+  // add to cart event handler
+  const addToCart = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+  };
+  return (
+    <div className="shop-Container">
+      <div className="product-container">
+        {products.map((product) => (
+          <Product
+            key={product.id}
+            product={product}
+            addToCart={addToCart}
+          ></Product>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
