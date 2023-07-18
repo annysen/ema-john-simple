fdc378f7d5f451182318544b4739d7052e27770b
annysen <annysen95@gmail.com>
2023-07-18 21:06:40 +0600

    get product id, find product using id, set product quantity

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,22 +2,39 @@ import React, { useEffect, useState } from "react";
 import "./Shop.css";
 import Product from "../Product/Product";
 import Cart from "../Cart/Cart";
+import { addToDb, getShoppingCart } from "../../assets/utilities/fakedb";
 const Shop = () => {
   const [products, setProducts] = useState([]);
 
   const [cart, setCart] = useState([]);
 
-  // load data fropm fetch
+  // load data from fetch
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
 
+  // load product and id from localStorage
+  useEffect(() => {
+    const storedCart = getShoppingCart();
+
+    // step-1: get product id using for in
+    for (const id in storedCart) {
+      // step-2: get product using id  using find method
+      const addedProduct = products.find((product) => product.id === id);
+      // step-3:  set the product quantity to product proparties
+      const quantity = storedCart[id];
+      addedProduct.quantity = quantity;
+      console.log(addedProduct);
+    }
+  }, [products]);
+
   // add to cart event handler
   const addToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id);
   };
   return (
     <div className="shop-Container">
